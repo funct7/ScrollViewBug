c38411c819c6fdbb666115c0c873a394f4d190ad
Josh Park <funct7.io@gmail.com>
2023-01-11 09:12:32 +0900

    Bug demo

--- ScrollViewBug/ViewController.swift
+++ ScrollViewBug/ViewController.swift
@@ -7,13 +7,34 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIScrollViewDelegate {
+    
+    @IBOutlet weak var xValueLabel: UILabel!
+    
+    @IBOutlet weak var contentInsetScrollView: UIScrollView!
+    
+    @IBOutlet weak var offsetPaddingScrollView: UIScrollView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        contentInsetScrollView.contentInset.left = .itemSize
+        // Try commenting out this line, and see how the behavior changes
+        contentInsetScrollView.contentInset.right = .itemSize
     }
-
-
+    
+    @IBAction func scrollAction(_ sender: UIButton) {
+        let x = CGFloat(Int(sender.title(for: .normal)!)!) * .itemSize
+        xValueLabel.text = "\(x)"
+        
+        [contentInsetScrollView, offsetPaddingScrollView].forEach {
+            $0.scrollRectToVisible(
+                CGRect(x: x, y: 0, width: .itemSize, height: .itemSize),
+                animated: true)
+        }
+    }
+    
 }
 
+private extension CGFloat {
+    static let itemSize: Self = 50
+}
